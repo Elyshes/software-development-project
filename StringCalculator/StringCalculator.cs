ac2315645e4c469ee3a05b52c5058b5ee5db51af
unknown <ni391fin@FHKN.DE>
2022-11-28 14:17:41 +0100

    added Unit Tests

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -3,13 +3,42 @@
 // (c) 2013 KARL STORZ GmbH and Co. KG.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+
 namespace Calculators
 {
     public class StringCalculator
     {
         public int Add(string s)
         {
-            return 0;
-        }
+            //check if string is empty and if true return -1 if not 0
+            //return string.IsNullOrEmpty(s) ? -1 : 0;
+            if(string.IsNullOrEmpty(s))
+            {
+                return -1;
+            }
+            int product = 1;
+            int sum = 0;
+            if (s.Contains("*"))
+            {
+                string[] SplitString = s.Split('*');
+                for (int NumbersInArray = 0; NumbersInArray < SplitString.Length; NumbersInArray++)
+                {
+                    product *= int.Parse(SplitString[NumbersInArray]);
+                }
+                return product;
+            }
+            else
+            {
+                string[] SplitString = s.Split(',');
+                for (int NumbersInArray = 0; NumbersInArray < SplitString.Length; NumbersInArray++)
+                {
+                    sum += int.Parse(SplitString[NumbersInArray]);
+                }
+                return sum;
+            }
+        }       
     }
 }
--- UnitTests/StringCalculatorTests.cs
+++ UnitTests/StringCalculatorTests.cs
@@ -8,9 +8,36 @@
 
     using Xunit;
     using Xunit.Extensions;
-
+    /* Create simple String calculator with a method int Add(string numbers)
+        The method can take 0, 1 or 2 numbers, and will retutn their sum(fir an empty string it will return -1)     
+     */
     public class StringCalculatorTests
     {
+        private StringCalculator objectUnderTest;
+
+        //constructer is setup for tests
+        //Arrange Act Assert = AAA
+        //only one Test for one Aspect
+        public StringCalculatorTests()
+        {
+            this.objectUnderTest = new StringCalculator();
+        }
+        // same as result.Should().Be(): Assert.Equal(0, result);
+
+        [Theory]
+        [InlineData("2,3", 5)]
+        [InlineData("1", 1)]
+        [InlineData("", -1)]
+        [InlineData("100,200", 300)]
+        [InlineData("2,3,80,980", 1065)]
+        [InlineData("6*9", 54)]
+        [InlineData("4*3*0", 0)]
+        public void WhenAddMulitpleNumbers(string numbers, int expectedresult)
+        {
+            var result = objectUnderTest.Add(numbers);
+
+            result.Should().Be(expectedresult);
+        }
 
     }
 }
